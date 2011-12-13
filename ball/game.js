00366c38aa40a7b1cd864015d6aa0133ae4a2661
Ivar Conradi Østhus <ivarconr@gmail.com>
2011-12-13 20:26:05 +0100

    corrected collision logic

--- ball/game.js
+++ ball/game.js
@@ -13,11 +13,9 @@ $(function() {
         //console.debug("pos.top=%s", "boundary.top=%s", pos.top, y);
 
         if(pos.top< 0 || pos.top > y) {
-	    speed.dy = speed.dy > 0 ? Math.floor(Math.random()*6) : Math.floor(Math.random()*6)*-1;
             speed.dy = speed.dy*-1;
         } 
         if(pos.left < 0 || pos.left > x)  {         
-	    speed.dx = speed.dx > 0 ? Math.floor(Math.random()*6) : Math.floor(Math.random()*6)*-1;
             speed.dx = speed.dx*-1;
         }
         $(".movable").css({"left": (pos.left + speed.dx) + "px", "top": (pos.top+speed.dy)+"px"});
